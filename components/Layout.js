bc7233a42bf9b6c08a9648afb5b5a2ad8e4dd286
Jose Arguelles <josearguelles@live.com.mx>
2023-02-17 11:00:40 -0600

    added Layout folder file and logic

---
+++ components/Layout.js
@@ -0,0 +1,20 @@
+const Layout = ({children}) => {
+        return (
+                <div>
+
+
+                        <h1>Desde Layout</h1>
+
+
+                        {children}
+                </div>
+
+
+
+
+
+
+
+        )
+}
+            export default Layout;
